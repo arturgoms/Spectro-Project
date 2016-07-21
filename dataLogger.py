4cdd872095378f0899a186c47048e29ebe939791
Artur Gomes <flashballclube@gmail.com>
2016-07-21 15:02:06 -0300

    Código agr separando a string e colocando em dois arquivos.

--- dataLogger.py
+++ dataLogger.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
 import serial
+from time import sleep
 
 def getSerial():
     try:
-        port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1.0)
+        port = serial.Serial('/dev/ttys0', baudrate=9600, timeout=1)
         while True:
-            with open("file.txt", "w") as f:
-                f.writelines(port)
-            f.close()
+            data = port.readline()
+            temperatura, altitude = data.split('separando')
+            with open ("temperatura.txt", "w") as f:
+                print temperatura
+                f.writelines(temperatura)
+                f.close()
+            with open ("humidade.txt", "w") as f:
+                print humidade
+                f.writelines(humidade)
+                f.close()
     except Exception as e:
-        print("Erro na Porta! %s" %e)
-
+        print("Erro' %s" %e)
 if __name__ == '__main__':
     getSerial()
