ed8888336b332ef3d15a38a2ecb3cc3c3bc8f4cc
Artur Gomes <flashballclube@gmail.com>
2016-07-24 12:57:22 -0300

    Envío de info para rasp ( Arduino Nano)

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+/*************************************************** 
+  This is an example for the BMP085 Barometric Pressure & Temp Sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/391
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+// EOC is not used, it signifies an end of conversion
+// XCLR is a reset pin, also not used here
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
+}
+  
+void loop() {
+    Serial.print(bmp.readTemperature());
+    Serial.print(";");
+    Serial.print(bmp.readPressure());
+    Serial.print(";");
+    Serial.print(bmp.readAltitude());
+    Serial.print(";");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.print(";");
+    Serial.print(bmp.readAltitude(101500));
+    delay(1000);
+}
